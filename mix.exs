73facb904efa8495e6140690afe7b411f18c255e
Kuznetsov Aleksey <alex.k@salam.io>
2018-08-24 14:35:26 +0300

    Configure dialyzer with proper options

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule EctoCassandra.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer(),
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -25,8 +32,21 @@ defmodule EctoCassandra.MixProject do
       {:dialyxir, "~> 1.0.0-rc.3", only: ~w(dev test)a, runtime: false},
       {:ecto, "~> 2.2"},
       {:ex_machina, "~> 2.2", only: :test},
+      {:excoveralls, "~> 0.8", only: :test},
       {:faker, "~> 0.10", only: :test},
+      {:mix_test_watch, "~> 0.7", only: :dev, runtime: false},
+      {:mox, "~> 0.4", only: :test},
       {:xandra, "~> 0.9"}
+    ]
+  end
+
+  # Dialyzer's configuration
+  defp dialyzer do
+    [
+      flags: ~w(unmatched_returns error_handling race_conditions underspecs unknown)a,
+      ignore_warnings: "config/dialyzer.ignore-warnings",
+      plt_add_apps: ~w(mix ex_unit)a,
+      plt_add_deps: :transitive
     ]
   end
 end
