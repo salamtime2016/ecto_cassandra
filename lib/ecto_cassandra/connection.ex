3d0e603dfd72fd866dad2ee3f3ab424eb70236f9
Kuznetsov Aleksey <alex.k@salam.io>
2018-08-24 17:28:55 +0300

    Initialize connection to Xandra

--- lib/ecto_cassandra/connection.ex
+++ lib/ecto_cassandra/connection.ex
@@ -1,1 +1,22 @@
+defmodule EctoCassandra.Connection do
+  @moduledoc """
+  Connect to Cassandra
+  """
 
+  @default_host "127.0.0.1"
+  @default_port 9042
+
+  @spec init(keyword) :: {:ok, pid}
+  def init(opts) when is_list(opts) do
+    [host, port] = [
+      Keyword.get(opts, :host, @default_host),
+      Keyword.get(opts, :port, @default_port)
+    ]
+
+    with nil <- Process.whereis(EctoCassandra.Conn),
+         {:ok, conn} <- Xandra.start_link(nodes: ["#{host}:#{port}"]),
+         true <- Process.register(conn, EctoCassandra.Conn) do
+      {:ok, conn}
+    end
+  end
+end
