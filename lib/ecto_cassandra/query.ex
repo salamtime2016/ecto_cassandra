6ca70049b091196f51e497b13130b9b2adda773f
Kuznetsov Aleksey <alex.k@salam.io>
2018-08-27 19:29:35 +0300

    Implement migration callbacks

--- lib/ecto_cassandra/query.ex
+++ lib/ecto_cassandra/query.ex
@@ -6,8 +6,13 @@ defmodule EctoCassandra.Query do
   alias Ecto.Query, as: Q
 
   @spec new(atom, Q.t()) :: String.t() | no_return
-  def new(:all, %Q{from: {table, _}, select: select} = query) do
+  def new(:all, %Q{from: {table, _}, select: select}) do
+    IO.inspect(select)
     "SELECT * FROM #{table}"
+  end
+
+  def new(:create_if_not_exists, table) do
+    IO.inspect(table)
   end
 
   def new(_, _) do
