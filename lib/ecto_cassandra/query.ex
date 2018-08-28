97b41e562dab8636c238efa266999b6e92f5d45a
Kuznetsov Aleksey <alex.k@salam.io>
2018-08-28 12:25:09 +0300

    Create a new tables

--- lib/ecto_cassandra/query.ex
+++ lib/ecto_cassandra/query.ex
@@ -7,12 +7,7 @@ defmodule EctoCassandra.Query do
 
   @spec new(atom, Q.t()) :: String.t() | no_return
   def new(:all, %Q{from: {table, _}, select: select}) do
-    IO.inspect(select)
     "SELECT * FROM #{table}"
-  end
-
-  def new(:create_if_not_exists, table) do
-    IO.inspect(table)
   end
 
   def new(_, _) do
