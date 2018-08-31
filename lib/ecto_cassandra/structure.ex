f2e8a9e3dd31177277a909399af05b84bbf3d0f5
neiro <alex.k@salam.io>
2018-08-31 15:51:47 +0300

    Respect auth opts from config on dump/dump load

--- lib/ecto_cassandra/structure.ex
+++ lib/ecto_cassandra/structure.ex
@@ -12,7 +12,7 @@ defmodule EctoCassandra.Structure do
     keyspace = Keyword.get(config, :keyspace)
     path = Path.join(default, @dump_file)
 
-    with {schema, 0} <- System.cmd("cqlsh", ["-e", "DESCRIBE #{keyspace}"]),
+    with {schema, 0} <- System.cmd("cqlsh", auth_opts(config) ++ ["-e", "DESCRIBE #{keyspace}"]),
          :ok <- File.write(path, schema) do
       {:ok, path}
     else
@@ -24,13 +24,20 @@ defmodule EctoCassandra.Structure do
   @spec structure_load(default :: String.t(), config :: Keyword.t()) ::
           {:ok, String.t()}
           | {:error, term}
-  def structure_load(default, _config) do
+  def structure_load(default, config) do
     path = Path.join(default, @dump_file)
 
-    with {_res, 0} <- System.cmd("cqlsh", ["-f", path]) do
+    with {_res, 0} <- System.cmd("cqlsh", ["-f", path] ++ auth_opts(config)) do
       {:ok, path}
     else
       {err, _} -> {:error, err}
+    end
+  end
+
+  defp auth_opts(config) do
+    case Keyword.get(config, :authentication) do
+      {_, [username: username, password: password]} -> ~w(-u #{username} -p #{password})
+      _ -> []
     end
   end
 end
