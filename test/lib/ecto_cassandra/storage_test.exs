b1e772a8c9278c646a1e162932b52eeb6436f2d0
neiro <neiro.mail@gmail.com>
2018-09-14 18:05:08 +0300

    Fix code quality issues

--- test/lib/ecto_cassandra/storage_test.exs
+++ test/lib/ecto_cassandra/storage_test.exs
@@ -1,1 +1,5 @@
+defmodule EctoCassandra.StorageTest do
+  @moduledoc false
 
+  use ExUnit.Case, async: true
+end
--- test/lib/ecto_cassandra/types_test.exs
+++ test/lib/ecto_cassandra/types_test.exs
@@ -1,1 +1,5 @@
+defmodule EctoCassandra.TypesTest do
+  @moduledoc false
 
+  use ExUnit.Case, async: true
+end
