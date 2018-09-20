9b401d478bb2958b749e6b5c6ff60e7e2b3025d4
Kuznetsov Aleksey <alex.k@salam.io>
2018-09-20 16:10:36 +0300

    Fix UUID generation

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule EctoCassandra.MixProject do
       {:faker, "~> 0.10", only: :test},
       {:mix_test_watch, "~> 0.7", only: :dev, runtime: false},
       {:mox, "~> 0.4", only: :test},
-      {:xandra, "~> 0.9"}
+      {:xandra, "~> 0.9"},
+      {:uuid, "~> 1.1"}
     ]
   end
 
